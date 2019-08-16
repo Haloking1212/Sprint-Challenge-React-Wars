ef30925ec1712353f808b2b160fe0e2fb8a72e2e
Hector Valdez <hvaldez180@gmail.com>
2019-08-16 17:27:14 -0400

    cleaned up commented code

--- starwars/src/StarCard.js
+++ starwars/src/StarCard.js
@@ -3,12 +3,10 @@ import React from "react";
 const StarCard = props => {
     console.log(props);
     return (
-        // <section className="star-wars">
         <div className="crawl star-wars" key={props.id}>
             <h3>Character: {props.character}</h3>
             <h2>Birthday: {props.birthyear}</h2>
         </div>
-        // </section>
     );
 };
 
