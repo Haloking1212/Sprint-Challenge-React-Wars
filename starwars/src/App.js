fe138b5c50eb9b451b5c2d1f2db072e37d09373f
Hector Valdez <hvaldez180@gmail.com>
2019-08-16 15:23:48 -0400

    added some styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import axios from "axios";
 import './App.css';
 import StarCard from "./StarCard";
-// import { Container, Header } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -23,7 +23,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <Container className="container"><h1 className="Header">React Wars</h1></Container>
       {data.map(dayta => {
         return (
           <StarCard 
--- starwars/src/StarCard.js
+++ starwars/src/StarCard.js
@@ -4,8 +4,8 @@ const StarCard = props => {
     console.log(props);
     return (
         <div className="film-list" key={props.id}>
-            <h2>Birthday: {props.birthyear}</h2>
             <h3>Character: {props.character}</h3>
+            <h2>Birthday: {props.birthyear}</h2>
         </div>
     );
 };
