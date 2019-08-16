ae143e1447c5152cfcb9e6690851ae9c3e01c2be
Hector Valdez <hvaldez180@gmail.com>
2019-08-16 17:24:35 -0400

    fixed animation bug

--- starwars/src/StarCard.js
+++ starwars/src/StarCard.js
@@ -3,12 +3,12 @@ import React from "react";
 const StarCard = props => {
     console.log(props);
     return (
-        <section className="star-wars">
-        <div className="crawl" key={props.id}>
+        // <section className="star-wars">
+        <div className="crawl star-wars" key={props.id}>
             <h3>Character: {props.character}</h3>
             <h2>Birthday: {props.birthyear}</h2>
         </div>
-        </section>
+        // </section>
     );
 };
 
